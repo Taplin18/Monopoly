984ab4165abfe44c760525a1eca2dac4828e298b
nimar21 <nimar21@users.noreply.github.com>
2017-02-28 01:01:15 +0000

    Update PlayerData.java

--- GUI-Niamh/PlayerData.java
+++ GUI-Niamh/PlayerData.java
@@ -11,19 +11,19 @@ public class PlayerData {
 	
   public PlayerData() {
 	
-	ArrayList<Player> playerData = new ArrayList<Player>(4); // Initialise storage structure for table contents
+	ArrayList<Client> playerData = new ArrayList<Client>(4); // Initialise storage structure for table contents
 	for (int i = 0; i < playerData.size(); i++) {
-	    // Add each client object to ArrayList at position i
+	    // Add each corresponding client object stored elsewhere to ArrayList at position i
 	}
 	  
 	Object columnNames[] = { "ID", "Username", "Money", "Properties", "Cards" }; // Define text for columns
-        playerStats = new JTable(playerData, columnNames); // Create JTable
-    	scrollPane = new JScrollPane(playerStats); // Create JScrollPane
+        //playerStats = new JTable(playerData, columnNames); // Create JTable
+    	//scrollPane = new JScrollPane(playerStats); // Create JScrollPane
 	  
-	// Create panel and add table to it
+	/* // Create panel and add table to it
 	playerDataPanel = new JPanel(new flowLayout());
 	playerDataPanel.setOpaque(true);
-	playerDataPanel.add(scrollPane);
+	playerDataPanel.add(scrollPane); */
   }
   
   public void populateTable() {
@@ -34,7 +34,7 @@ public class PlayerData {
 		// Get money at position i and add to third column
 		// Get properties at position i owned and add to fourth column
 		// Get GOOJ Card info at position i and add to fifth column
-		row++; // Move to next row
+		rowNumber++; // Move to next row
 	}
   }
 } 
