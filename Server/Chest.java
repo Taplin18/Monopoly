4b3fc8c857f3cdbb69d7b2d0c0456847c4a251a4
Lauren Taplin <laurentap96@gmail.com>
2017-03-01 17:12:06 +0000

    updates

--- Server/Chest.java
+++ Server/Chest.java
@@ -26,7 +26,7 @@ class Chest {
 	*/
 	private void make_chest_cards() {
 		cards.add("chest - add - 200 - Bank error in your favour -> Collect €200");
-		cards.add("chest - pay - 50 - Doctor's fee -> Pay €50");
+		cards.add("chest - pay - -50 - Doctor's fee -> Pay €50");
 		cards.add("chest - add - 50 - From sale of stock you get €50");
 		cards.add("chest - jail - out - Get Out of Jail Free");
 		cards.add("chest - jail - in - Go to Jail");
@@ -35,8 +35,8 @@ class Chest {
 		cards.add("chest - add - 20 - Income tax refund -> Collect €20");
 		cards.add("chest - add - 10 - It's your birthday -> Collect €10");
 		cards.add("chest - add - 100 - Life insurance matures -> Collect €100");
-		cards.add("chest - pay - 100 - Pay hospital fees of €100");
-		cards.add("chest - pay - 150 - Pay school fees of €150");
+		cards.add("chest - pay - -100 - Pay hospital fees of €100");
+		cards.add("chest - pay - -150 - Pay school fees of €150");
 		cards.add("chest - add - 25 - Receive €25 consultancy fee");
 		cards.add("chest - add - 10 - You have won second place in a beauty contest -> Collect €10");
 		cards.add("chest - add - 100 - You inherit €100");
