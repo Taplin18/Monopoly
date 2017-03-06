ece7de848fb74e44a2c21e8f71d453d471ea0cdc
stephenhannon01 <stephenhannon01@gmail.com>
2017-03-06 19:35:07 +0000

    Update Client.java

--- Client/Client.java
+++ Client/Client.java
@@ -680,6 +680,7 @@ public class Client{
       exception.printStackTrace();
     }
     Client client= new Client();
+	  System.out.println("I done got created ");
 	JFrame myFrame = new JFrame("Welcome to Zebropoly!"); // Create new JFrame with specified name
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Specifies that the application must exit when window is closed
         WelcomeScreen welcomeScreen = new WelcomeScreen(myFrame); // Create instance of WelcomeScreen 
@@ -689,8 +690,11 @@ public class Client{
         myFrame.setSize(550, 500);
 	myFrame.setLocationRelativeTo(null);
         myFrame.setVisible(true); // Window is displayed
+	  System.out.println("frame done got made ");
 	while (welcomeScreen.getUsername().equals("")){}
+	  System.out.println("while loop done got exited ");
 	client.setUsername(welcomeScreen.getUsername());
+	  System.out.println(" username gone done gotten and set hopefree");
 	System.out.println("Username: "+client.getUserName());
 	boolean start=false;
 	while(!start){
