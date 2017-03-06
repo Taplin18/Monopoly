d4a09a34567f2e9e556b9b50d9530d58bcf5e7b4
crowledg <114733575@umail.ucc.ie>
2017-03-06 18:17:26 +0000

    Update Player.java

--- GUI - Denise/Player.java
+++ GUI - Denise/Player.java
@@ -1,4 +1,5 @@
 
+
 package monopoly3;
 
 import java.awt.Graphics;
@@ -8,9 +9,16 @@ import java.io.IOException;
 import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
-
+/**
+ * The player class assigns the image corresponding to the player and moves the player around the board
+ * @author cascadafreak07
+ */
 public  class Player {
 
+    /**
+     * A constructor which takes in the Points class to assign the coordinates on boards
+     * @param point
+     */
     public Player(Points point){
         this.xPosition = point.getX();
         this.yPosition = point.getY();
@@ -20,11 +28,19 @@ public  class Player {
     private int yPosition;
     BufferedImage player;
    
+    /**
+     * Moves the player position on board
+     * @param points
+     */
     public void move(Points points){
         this.xPosition = points.getX();
         this.yPosition = points.getY();
     } 
     
+    /**
+     * Returns the array of possible images for the player
+     * @return players
+     */
     public BufferedImage[] getPlayers(){
         BufferedImage [] players = new BufferedImage[8];
         BufferedImage img;
@@ -40,14 +56,27 @@ public  class Player {
       
         return players;
     }
+
+    /**
+     * Return the buffered image icon of the player
+     * @return player
+     */
     public BufferedImage getPlayer() {
         return player;
     }
 
+    /**
+     * Assign a buffered image to the player
+     * @param player
+     */
     public void setPlayer(BufferedImage player) {
         this.player = player;
     }
 
+    /**
+     * Draw the player image icon in the given x and y coordinate
+     * @param g
+     */
     public void draw(Graphics g) {
 
         g.drawImage(getPlayer(), xPosition, yPosition, null);
